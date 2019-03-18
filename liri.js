021bd9f2dcd9c2d7dda6cd52ec8062ad9ab9871c
Margarita Ilchenko <multik55555@gmail.com>
2019-03-18 00:13:16 -0400

    created content

---
+++ liri.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+const result = dotenv.config()
+
+var concert = require("./concert-this.js");
+var whatItSays = require("./do-what-it-says.js");
+var spotify = require("./spotify-this-song.js");
+var movie = require("./movie-this.js");
+ 
+if (command === "concert-this") {
+    do these;
+} else if (command === "spotify-this-song") {
+    do these;
+} else if (command === "movie-this") {
+    do these;
+} else if (command === "do-what-it-says") {
+    do these;
+} else {
+
+}
+ 
