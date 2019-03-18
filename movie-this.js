0249ab54bfbb6a0bd82dced774054ab9e619d5a4
Margarita Ilchenko <multik55555@gmail.com>
2019-03-18 09:39:04 -0400

    added movie axios

--- movie-this.js
+++ movie-this.js
@@ -1,2 +1,15 @@
  
 module.exports = movie;
+
+var axios = require("axios");
+
+var movie = process.argv[2];
+
+// Then run a request with axios to the OMDB API with the movie specified
+axios.get("http://www.omdbapi.com/?t=" + movie.replace(' ', '+') + "&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    console.log("title " + response.data.imdbTitle);
+    console.log("The movie's rating is: " + response.data.imdbRating);
+    
+  }
+);
