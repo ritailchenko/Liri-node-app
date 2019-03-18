8c8acefda5d9f7e90df1cef64925134c4ecb70a0
Margarita Ilchenko <multik55555@gmail.com>
2019-03-18 09:49:17 -0400

    added axios to concert

--- concert-this.js
+++ concert-this.js
@@ -1,2 +1,15 @@
  
 module.exports = concert;
+
+var axios = require("axios");
+
+var concert = process.argv[2];
+
+// Then run a request with axios to the OMDB API with the movie specified
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+  function(response) {
+    console.log("title " + response.data.Name);
+    console.log("The movie's rating is: " + response.data.Location);
+    console.log("The movie's rating is: " + response.data.Date);
+  }
+);
